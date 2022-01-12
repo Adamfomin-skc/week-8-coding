9cb11562f093d1777feb19ed47fa43bf1a316332
Adamfomin-skc <96421014+Adamfomin-skc@users.noreply.github.com>
2022-01-12 10:37:39 +0000

    Add files via upload

---
+++ week 8 coding/While and for loop 151121.py
@@ -0,0 +1,21 @@
+import random
+number = random.randint(5,20)
+y = 0
+t = random.randint(1,10)
+while y <= number: 
+    
+    x = 0    
+    if x <= number:
+        print('She loves me')
+        x += 1
+while t > y:
+    if t <= number:
+        print('She loves me not')
+        x += 1
+    t =  t + y
+    
+
+    
+
+
+
